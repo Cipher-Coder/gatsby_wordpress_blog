48df6c63cf448e518d5420f368b4e87cc7abb5eb
Matthew Rebehn <matthew.rebehn@gmail.com>
2021-12-16 09:56:53 -0500

    Updated global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,5 @@ import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/css/normalize.css"
 
-// custom CSS styles
-// import "./src/css/style.css"
-
 // importing tailwind globals
 import "./src/css/global.css"
